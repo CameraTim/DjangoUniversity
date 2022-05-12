0bcd74a3e171c9076868338e823d0f6489ddebcc
CameraTim <senekerisme@gmail.com>
2022-05-12 00:01:54 -0400

    Update

--- Classes/classApp/models.py
+++ Classes/classApp/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Creating the form structure for the app
 class djangoClasses(models.Model):
     title = models.CharField(max_length=50)
-    courseNumber = models.IntegerField()
+    courseNumber = models.IntegerField(default=0)
     instructor = models.CharField(max_length=30)
-    duration = models.DecimalField(decimal_places=3, max_digits=10000)
+    duration = models.FloatField(default=0)
 
     # Adding a class manager
     objects = models.Manager()
@@ -14,8 +14,14 @@ class djangoClasses(models.Model):
     def __str__(self):
         return self.title
 
+
 # Creating variables from the class objects and assigning values to them
-classVar = djangoClasses()
-x = classVar.title
-y = classVar.courseNumber
-z = classVar.instructor
+object1 = djangoClasses(title="SomeObject", courseNumber=1, instructor="Billy", duration=2.5)
+object1.save()
+
+object2 = djangoClasses(title="anotherObject", courseNumber=2, instructor="BillyBob", duration=60)
+object2.save()
+
+object3 = djangoClasses(title="thisObject", courseNumber=3, instructor="Joe", duration=30)
+object3.save()
+
